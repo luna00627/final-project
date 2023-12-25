148a9a6f57e395c8c3ece28fb7b8adb652c4548d
luna00627 <az991130627@gmail.com>
2023-12-26 03:45:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -175,7 +175,7 @@ app.post("/login", async (req, res) => {
 });
 
 
-const port = 5000;
+const port = process.env.PORT||5000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
